cf4f55a77a9f592d7a666aef34b7b9539a0bbf1c
PapaPingouin <papapingouin@imw.fr>
2013-09-22 15:56:02 +0200

    commit 2

--- moc_qtsingleapplication.cpp
+++ moc_qtsingleapplication.cpp
@@ -7,7 +7,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../qt-solutions-qt-solutions/qtsingleapplication/src/qtsingleapplication.h"
+#include "qtsingleapplication/qtsingleapplication.h"
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'qtsingleapplication.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 63
